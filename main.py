340df61110f4fff1811598d58cb5c9e0f5958e28
DOMI-103 <taka11one@gmail.com>
2026-03-01 22:13:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
